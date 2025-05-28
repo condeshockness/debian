00d04cf1e4c44a825247448c10e8dde38337196d
Alfonsin <ramonsilas.dev@gmail.com>
2025-05-28 18:54:51 -0400

    ok

---
+++ 00_lxc-details.sh
@@ -0,0 +1,12 @@
+#echo -e "[1mConteiner Debian[m" 
+
+echo -e "[1mDebian virtalbox[m" 
+echo -e "[1mRamon Silas[m" 
+echo -e "[1mSO para Redes Livres I[m" 
+echo -e "[1mProf°. Tiago[m" 
+
+echo -e "    📦  [m[33m Conteiner: [1;92m${OS_NAME} [33m          | Versão: [1;92m${OS_VERSION}[m" 
+
+echo -e "    🆔  [m[33m Hostname : [1;92m$(hostname)[m" 
+
+echo -e "    🌐  [m[33m Endereço : [1;92m$(hostname -I | awk '{print $1}')[m" 
